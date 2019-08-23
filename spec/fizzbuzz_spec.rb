4c41fd357476000a31cc4eaa80fbc47d36960658
Derek Baker <derek.baker89@gmail.com>
2019-08-23 10:58:03 +0100

    change first test to reflect multiples of 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,8 +2,8 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   
-   it 'returns "fizz" when passed 3' do 
-    expect(fizzbuzz(3)).to eq 'fizz'
+   it 'returns "fizz" when number passed is divisible by 3' do 
+    expect(fizzbuzz(9)).to eq 'fizz'
    end
 
    it 'returns "buzz" when passed 5' do
