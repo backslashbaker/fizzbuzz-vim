7173b2cc5e010a006276641591c91cfdfb20c6cd
Derek Baker <derek.baker89@gmail.com>
2019-08-23 11:07:34 +0100

    create test for instances of both multiples of 3 and 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,13 +6,18 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(9)).to eq 'fizz'
    end
 
-   it 'returns "buzz" when passed 5' do
+   it 'returns "buzz" when number passed is divisible by 5' do
      expect(fizzbuzz(5)).to eq 'buzz'
    end
    
    it 'returns the number' do 
      expect(fizzbuzz(1)).to eq 1
    end
+
+   it 'returns "fizzbuzz" when number passed is both divisible by 3 and 5' do
+     expect(fizzbuzz(15)).to eq 'fizzbuzz'
+   end
+
  
 end
 
